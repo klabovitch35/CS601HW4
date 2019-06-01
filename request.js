c85840d5ca93a7c4924b28b9669d3b6707e126cb
klabovitch <klabovitch@gmail.com>
2019-06-01 12:09:44 -0400

    parse test

--- request.js
+++ request.js
@@ -2,12 +2,10 @@
     var httpRequest;
     document.querySelector("button").onclick = function() { makeRequest('https://cdn.jsdelivr.net/gh/klabovitch35/CS601HW4@master/source.json');
     };
-    alert("it works");
     function makeRequest(url) {
-        alert("in make request");
         httpRequest = new XMLHttpRequest();
         if(!httpRequest) {
-            alert('Failed to create httpRequest');
+            alert('Failed to create HTTPRequest');
             return false;
         }
         httpRequest.onreadystatechange = jsonContent;
@@ -15,18 +13,24 @@
         httpRequest.send(); 
 
     }
+
     function jsonContent() {
         alert("in jsonContent");
         alert("ready state: " + httpRequest.readyState);
         if(httpRequest.readyState === XMLHttpRequest.DONE) {
             if(httpRequest.status === 200) {
                 alert("successful request");
-                document.write(httpRequest.responseText);
+                var response = JSON.parse(httpRequest.responseText);
+                document.write(response);
+                JSON.parse(response)
             } else {
-                alert("no good request");
+                alert("failed request");
             }
         }
-        alert("done with json content");
+    }
+
+    function createTable() {
+
     }
 
 }());
