669e10529334fee6e3265e4c2d31b04212ceea7a
klabovitch <klabovitch@gmail.com>
2019-06-01 11:07:48 -0400

    execute

---
+++ request.js
@@ -0,0 +1,34 @@
+(function () {
+    var httpRequest;
+    document.querySelector("button").onclick = function() { makeRequest('https://api.myjson.com/bins/i59fv');
+    };
+    alert("it works");
+    function makeRequest(url) {
+        alert("in make request");
+        httpRequest = new XMLHttpRequest();
+        if(!httpRequest) {
+            alert('Failed to create httpRequest');
+            return false;
+        }
+        httpRequest.onreadystatechange = jsonContent;
+        httpRequest.open('GET', url);
+        httpRequest.send(); 
+
+    }
+    function jsonContent() {
+        alert("in jsonContent");
+        alert("ready state: " + httpRequest.readyState);
+        if(httpRequest.readyState === XMLHttpRequest.DONE) {
+            if(httpRequest.status === 200) {
+                alert("successful request");
+                document.write(httpRequest.responseText);
+            } else {
+                alert("no good request");
+            }
+        }
+        alert("done with json content");
+    }
+
+}());
+        
+  
